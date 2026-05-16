9d668e60cfb04c1f64fe9759730117e4983fb1f7
putrianindyahapsari <putrianindyahapsari1@gmail.com>
2026-05-16 22:34:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'dark-bg': '#050112',
+                        'purple-primary': '#6D28D9',
+                        'purple-light': '#A78BFA',
+                        'glass': 'rgba(255, 255, 255, 0.03)',
+                    },
+                    fontFamily: {
+                        sans: ['Plus Jakarta Sans', 'sans-serif'],
+                    }
+                }
+            }
+        }
